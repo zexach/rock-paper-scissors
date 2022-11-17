5ee5deea9232cfcc24ceeed33e024b31c8681700
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2022-11-17 18:36:41 +0100

    Swapped images for scissors and rock

--- src/components/OpponentPick.vue
+++ src/components/OpponentPick.vue
@@ -9,8 +9,8 @@
     <div class="opponent-pick">
         <h3>OPPONENT'S PICK</h3>
         <Choice choice="https://i.imgur.com/B9bhcFt.png" style="border: 15px solid #2ce014" v-if="opponentPick === 'paper'" />
-        <Choice choice="https://i.imgur.com/hYMBReL.png" style="border: 15px solid #ffd70d" v-if="opponentPick === 'rock'" />
-        <Choice choice="https://i.imgur.com/o41ORt9.png" style="border: 15px solid red" v-if="opponentPick === 'scissors'" />
+        <Choice choice="https://i.imgur.com/o41ORt9.png" style="border: 15px solid #ffd70d" v-if="opponentPick === 'rock'" />
+        <Choice choice="https://i.imgur.com/hYMBReL.png" style="border: 15px solid red" v-if="opponentPick === 'scissors'" />
     </div>
 </template>
 
